69aeef282fa077499674f94d315bf626383bc01f
huangjuhua <952172047@qq.com>
2021-12-01 18:36:00 +0800

    feat: :zap: 自动导入 ui components

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
+const Components = require('unplugin-vue-components/webpack')
+const { ElementPlusResolver } = require('unplugin-vue-components/resolvers')
+
 module.exports = {
+  plugins: [
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+  ],
   devServer: {
     port: 8080,
     headers: {
