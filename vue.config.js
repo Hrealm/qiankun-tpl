59dd15a228f03e0d4f1ac6e39d3aa3c58217be6f
huangjuhua <952172047@qq.com>
2021-12-02 10:45:25 +0800

    feat: :rocket: webpack配置自动导入ui 组件

--- vue.config.js
+++ vue.config.js
@@ -2,11 +2,13 @@ const Components = require('unplugin-vue-components/webpack')
 const { ElementPlusResolver } = require('unplugin-vue-components/resolvers')
 
 module.exports = {
-  plugins: [
-    Components({
-      resolvers: [ElementPlusResolver()],
-    }),
-  ],
+  configureWebpack: {
+    plugins: [
+      Components({
+        resolvers: [ElementPlusResolver()],
+      }),
+    ],
+  },
   devServer: {
     port: 8080,
     headers: {
